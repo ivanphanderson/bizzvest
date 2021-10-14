c4f1636afe50f1894b55b075d41e12a040bfed64
azzam.labib <azzam.labib@ui.ac.id>
2021-10-14 16:06:23 +0700

    rename app and branch to Snake case

--- daftar_toko/apps.py
+++ daftar_toko/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DaftartokoConfig(AppConfig):
+class DaftarTokoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'daftarToko'
+    name = 'daftar_toko'
