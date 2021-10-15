86ada476a1e013a1360a6f90759407fb8d3cc69c
Nandhita Zefania <nandhita.zefania@ui.ac.id>
2021-10-15 09:09:06 +0700

    update app faq

--- faq/models.py
+++ faq/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Faq(models.Model):
+    nama = models.CharField(max_length=30)
     pertanyaan = models.TextField()
-    jawaban = models.TextField()
