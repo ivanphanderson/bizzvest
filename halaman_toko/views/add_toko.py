e6c4f485a23032bd948fd74697f30749adeeed51
nuel <32788259+Hzzkygcs@users.noreply.github.com>
2021-10-26 18:21:11 +0700

    tambahkan todo

--- halaman_toko/views/add_toko.py
+++ halaman_toko/views/add_toko.py
@@ -1,5 +1,5 @@
 from django.core.handlers.wsgi import WSGIRequest
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.urls import reverse
 from django.utils import dateformat, timezone
@@ -36,61 +36,6 @@ class FormErrors():
             setattr(self.does_problem_exist, attr_name, "problem" if (attr_name in dictionary) else "no-problem")
 
 
-"""
-string_character_choices = string.digits + string.ascii_letters
-class TemporaryFiles:
-    global_dict = {}
-    global_queue = deque()
-
-    def __init__(self, data, timeout_in_second=12):
-        while (id:=''.join(random.choices(string_character_choices, k=48))) in self.__class__.global_dict:
-            pass
-
-        self.data = data
-        self.id = id
-        self.timeout = datetime.datetime.now() + datetime.timedelta(0, timeout_in_second)
-        self.__class__.global_dict[self.id] = self
-        self.__class__.global_queue.append(self)
-
-    @classmethod
-    def get(cls, key, default=Exception):
-        ret = cls.global_dict.get(key, default)
-        if isinstance(ret, Exception):
-            raise ret("key error")
-        return ret
-
-
-    @classmethod
-    def delete_outdated_files(cls):
-        if len(cls.global_queue) == 0:
-            assert len(cls.global_dict) == 0
-            return
-
-        temp = cls.global_queue[-1]
-        while (temp.timeout < datetime.datetime.now()):
-            if temp.id in cls.global_dict:
-                del cls.global_dict[temp.id]
-                Company.objects.filter(id=temp.data).delete()
-            cls.global_queue.pop()
-
-            if not cls.global_queue:  # if queue is empty, then dict must be empty. Tapi ga berlaku sebaliknya
-                assert len(cls.global_dict) == 0
-                break
-
-            temp = cls.global_queue[-1]
-
-    @classmethod
-    def mark_as_permanent(cls, tempfile_id):
-        del cls.global_dict[tempfile_id]
-
-    @classmethod
-    def reset_counter(cls, prev_id):
-        ret = cls(cls.global_dict[prev_id].data)
-        del cls.global_dict[prev_id]
-        return ret
-"""
-
-
 
 def add_toko(req:WSGIRequest):
     # TemporaryFiles.delete_outdated_files()
@@ -102,6 +47,7 @@ def add_toko(req:WSGIRequest):
     if (get_logged_in_user_account() is None):
         return HttpResponseRedirect(get_login_url())
 
+    # TODO: Cek apakah akun pengguna sudah terverifikasi (KTP dsb) atau belum
 
     if (req.method == 'POST'):
 
@@ -111,8 +57,10 @@ def add_toko(req:WSGIRequest):
         temp = get_logged_in_user_account().entrepreneuraccount
         form.instance.pemilik_usaha = temp
 
-        # if ('pemilik_usaha' in req.POST):
-        #     return HttpResponse("Illegal attribute: 'pemilik_usaha' ", status=400)
+        if ('pemilik_usaha' in req.POST):
+            return HttpResponse("Illegal attribute: 'pemilik_usaha' ", status=400)
+
+
 
         if ('is_validate_only' not in req.POST):
             additional_problems.append("invalid request error: no 'is_validate_only' property")
