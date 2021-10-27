d17852156cf4c0b85cb2bd5728d75a84a93fcdf0
azzam.labib <azzam.labib@ui.ac.id>
2021-10-27 20:47:21 +0700

    selesai solve masalah pemanggilan foto ke card daftar toko

--- daftar_toko/views.py
+++ daftar_toko/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from models_app.models import Company
+from halaman_toko.views.utility import validate_toko_id_by_GET_req, validate_toko_id
+
 # Create your views here.
 
 def tampilkan_toko(request):
-    context = {}
-    return render(request, 'index_daftar_toko.html')
+    company = Company.objects.all().values()
+    company_obj = Company.objects.all()
+    return render(request, 'index_daftar_toko.html', {'company': company, 'company_obj': company_obj})
+
+def insert_photos_dict():
+    company = Company.objects.all()
