f0afaa699c7f8d45adf29ccd339aa901d1967673
azzam.labib <azzam.labib@ui.ac.id>
2021-10-28 12:21:33 +0700

    fixing some bugs even though still fail, add pagination

--- daftar_toko/views.py
+++ daftar_toko/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from models_app.models import Company
 from halaman_toko.views.utility import validate_toko_id_by_GET_req, validate_toko_id
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
 def tampilkan_toko(request):
-    company = Company.objects.all().values()
-    company_obj = Company.objects.all()
-    return render(request, 'index_daftar_toko.html', {'company': company, 'company_obj': company_obj})
-
-def insert_photos_dict():
     company = Company.objects.all()
+
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(company, 2)
+    try:
+        company_obj = paginator.page(page)
+    except PageNotAnInteger:
+        company_obj = paginator.page(1)
+    except EmptyPage:
+        company_obj = paginator.page(paginator.num_pages)
+
+    return render(request, 'index_daftar_toko.html', {'company_obj': company_obj})
+
