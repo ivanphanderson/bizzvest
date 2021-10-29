3395bbf8d9fc56ec53c18e1c0ce95a2d96e2bb9a
nuel <32788259+Hzzkygcs@users.noreply.github.com>
2021-10-29 09:07:23 +0700

    add test coverage

--- halaman_toko/tests.py
+++ halaman_toko/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import resolve
+from django.http import HttpRequest
 
 # Create your tests here.
+
+class HalamanTokoTest(TestCase):
+    def test_halaman_toko_response(self):
+        response = Client().get('/halaman-toko/')
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_manage_toko_response(self):
+        response = Client().get('/halaman-toko/edit-photos')
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_add_toko(self):
+        response = Client().get('/halaman-toko/add')
+        self.assertNotEqual(response.status_code, 302)  # karena belum login, diarahkan ke halaman login
+
