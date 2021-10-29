bb4f5b2ffb5ceb3342a75cff937c5d91441c4a3a
nuel <32788259+Hzzkygcs@users.noreply.github.com>
2021-10-29 09:35:22 +0700

    .

--- halaman_toko/tests.py
+++ halaman_toko/tests.py
@@ -1,10 +1,30 @@
 from django.test import Client, TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from models_app import models
 
 # Create your tests here.
 
+
+
 class HalamanTokoTest(TestCase):
+    def test_halaman_toko_response(self):
+        response = Client().get('/halaman-toko/')
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_manage_toko_response(self):
+        response = Client().get('/halaman-toko/edit-photos')
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_add_toko(self):
+        response = Client().get('/halaman-toko/add')
+        self.assertEqual(response.status_code, 302)  # karena belum login, diarahkan ke halaman login
+
+
+class HalamanTokoSudahLoginTest(TestCase):
+    def setUp(self) -> None:
+        UserAccount.objects.create()
+
     def test_halaman_toko_response(self):
         response = Client().get('/halaman-toko/')
         self.assertNotEqual(response.status_code, 200)
