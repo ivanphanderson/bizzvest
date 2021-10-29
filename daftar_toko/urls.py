5bc2dfef01d6cdf7c231fb587d24c11acdfa74ba
azzam.labib <azzam.labib@ui.ac.id>
2021-10-29 13:26:25 +0700

    styling change and card model change

--- daftar_toko/urls.py
+++ daftar_toko/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 
 urlpatterns = [
-    path('', views.tampilkan_toko, name="daftar_toko"),
+    path('', views.tampilkan_toko, name="tampilkan_toko"),
+    url(r'^search/$', views.search, name='search'),
 ]
