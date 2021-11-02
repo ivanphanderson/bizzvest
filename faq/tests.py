e29c897de0ea44428437ecde36e5447be35b4e39
Nandhita Zefania <nandhita.zefania@ui.ac.id>
2021-11-02 19:03:33 +0700

    update test.py

--- faq/tests.py
+++ faq/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.test import Client
+from django.urls import resolve
+from .views import *
 
-# Create your tests here.
+
+class FAQTest(TestCase):
+
+    def test_faq_is_exist(self):
+        response = Client().get('/faq/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_using_index_func(self):
+        found = resolve('/faq/')
+        self.assertEqual(found.func, index)
+
+    def test_faq_attributes(self):
+        obj1 = Faq.objects.create(nama='Nandhita Zefana Maharani', pertanyaan='Apakah keuntungan dari mengikuti BizzVest?')
+        self.assertEqual(obj1.nama, 'Nandhita Zefana Maharani')
+        self.assertEqual(obj1.pertanyaan, 'Apakah keuntungan dari mengikuti BizzVest?')
