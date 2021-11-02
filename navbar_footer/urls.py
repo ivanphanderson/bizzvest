49dc3f965ca3c84f7d33d4215a0690fe9f4306a5
Raissa Tito Safaraz <raissa.tito@ui.ac.id>
2021-11-02 21:48:34 +0700

    update a bit

--- navbar_footer/urls.py
+++ navbar_footer/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import index
 
 urlpatterns = [
-    path('home', views.home, name='home')
+    path('', index, name='index')
 ]
