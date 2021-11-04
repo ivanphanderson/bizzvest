b970545dbb1dcfffe4a268c140540473fbbdde0f
raihansyahyoga11 <raihansyahyoga@gmail.com>
2021-11-04 15:47:23 +0700

    perubahan sedikit setelah merge ke main

--- home_page/models.py
+++ home_page/models.py
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 from django.db import models
 # 
 # # Create your models here.
 # class Message(models.Model):
 #     email = models.CharField(max_length=30)
 #     message = models.CharField(max_length=200)
-=======
-from django.db import models
-
-# Create your models here.
->>>>>>> yoga_my_profile
