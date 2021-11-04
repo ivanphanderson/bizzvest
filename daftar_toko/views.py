2eb83a9f63236408c52af67fc42d9e6b949b5d54
azzam.labib <azzam.labib@ui.ac.id>
2021-11-04 19:36:43 +0700

    fixing search bar and aestheticness :v

--- daftar_toko/views.py
+++ daftar_toko/views.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http.response import HttpResponse
 from django.core import serializers
-from django.template.context_processors import csrf
+from django.views.generic import ListView
 
 # Create your views here.
 
@@ -14,7 +14,7 @@ def tampilkan_toko(request):
 
     page = request.GET.get('page', 1)
 
-    paginator = Paginator(company, 6)
+    paginator = Paginator(company, 15)
     try:
         company_obj = paginator.page(page)
     except PageNotAnInteger:
@@ -25,15 +25,20 @@ def tampilkan_toko(request):
     return render(request, 'index_daftar_toko.html', {'company_obj': company_obj})
 
 def search(request):
+    company_search = []
+    search_text = ''
     if request.method == "POST":
         search_text = request.POST['search_text']
+        company_search = list(Company.objects.filter(Q(nama_perusahaan__contains=search_text)|Q(nama_merek__contains=search_text)|Q(kode_saham__contains=search_text)).values())       
     else:
-        search_text = ''
-
-    company_search = list(Company.objects.filter(Q(nama_perusahaan__contains=search_text)|Q(nama_merek__contains=search_text)|Q(kode_saham__contains=search_text)).values())
+        company_search = list(Company.objects.all().values())
+    
+    for company in company_search:
+        company["img"] = Company.objects.get(id=company["id"]).companyphoto_set.all().order_by('img_index').first().img.url
 
     return JsonResponse({'company_search' : company_search})
 
 def experiment(request):
     data = serializers.serialize('json', Company.objects.all())
     return HttpResponse(data, content_type="application/json")
+
