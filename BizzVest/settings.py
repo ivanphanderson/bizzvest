c105b80dd2e5463455c3ab92673ab13109cb26de
nuel <32788259+Hzzkygcs@users.noreply.github.com>
2021-11-04 22:39:59 +0700

    .

--- BizzVest/settings.py
+++ BizzVest/settings.py
@@ -41,6 +41,7 @@ ALLOWED_HOSTS = ['*', 'localhost', 'bizzvest.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
