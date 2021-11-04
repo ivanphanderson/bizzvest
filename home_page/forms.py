92321e1b00c8c547a9bce266fbd2557b512b5724
Ahmad Ghozi Fidinillah <ahmadghozi1234@gmail.com>
2021-11-04 12:23:22 +0700

    menambah modal berisi form kirim pesan, namun belum ada model

---
+++ home_page/forms.py
@@ -0,0 +1,8 @@
+# from home_page.models import Pesan
+# from django.forms import ModelForm
+# 
+# 
+# class PesanForm(ModelForm):
+#     class Meta:
+#         model = Pesan
+#         fields = "__all__"
--- home_page/models.py
+++ home_page/models.py
@@ -1,3 +1,6 @@
-from django.db import models
-
-# Create your models here.
+# from django.db import models
+# 
+# # Create your models here.
+# class Pesan(models.Model):
+#     email = models.CharField(max_length=30)
+#     message = models.CharField(max_length=200)
