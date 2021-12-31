e07df67a047bd25600f6e622ebb0ddeff4a50bfa
Ivan Phanderson <ivan.phanderson@ui.ac.id>
2021-12-31 08:31:28 +0000

    Ivan mulai invest

--- mulai_invest/static/js/saldo_saham.js
+++ mulai_invest/static/js/saldo_saham.js
@@ -1,17 +1,15 @@
-var saham_sisa = parseInt(document.getElementById("saham_tersisaa").textContent);
-var saldo = parseInt(document.getElementById("user_saldo").textContent);
-
-
-function update_saldo() {
-  saldo_temp = saldo;
+function update_saldo2(data) {
+  saldo_temp = data.saldo;
   formatted_saldo = (saldo_temp).toLocaleString('ID', {
     style: 'currency',
     currency: 'IDR',
   });
   document.getElementById("saldoSekarang").innerHTML = formatted_saldo;
-  $( ".refresh" ).load(window.location.href + " .refresh" );
-  $( ".refresh1" ).load(window.location.href + " .refresh1" );
-  $( ".refresh2" ).load(window.location.href + " .refresh2" );
+  document.getElementById("lembar_dimiliki").innerHTML = data.saham_tanam + " Lembar";
+  document.getElementById("saham_terjual").innerHTML = data.saham_terjual + " lembar";
+  document.getElementById("saham_tersisa").innerHTML = data.saham_tersisa + " lembar";
+
+  $( ".user-saldo" ).load(window.location.href + " .user-saldo" );
 }
 
 function load_DER() {
@@ -21,7 +19,7 @@ function load_DER() {
     style: 'currency',
     currency: 'IDR',
   }));
-  document.getElementById("harga_sahams").innerHTML =harga_saham_format;
+  document.getElementById("harga_sahams").innerHTML = harga_saham_format;
 }
 
 var formatter = new Intl.NumberFormat('ID', {
@@ -30,53 +28,37 @@ var formatter = new Intl.NumberFormat('ID', {
 });
 
 
-
-
 window.onload = load_DER();
 
 $("form#beli_saham").submit(function() {
   var saham_dibeli = $('input[name="jumlah_lembar_saham"]').val().trim();
-  var sisa_lembar = parseInt(saham_sisa)-parseInt(saham_dibeli);
-  var totalHarga = parseInt(saham_dibeli)*parseInt(document.getElementById("company_nilai_saham").textContent);
-  var saldo_temp = parseInt(saldo)-parseInt(totalHarga);
   var id_comp= parseInt(document.getElementById("company_id").textContent);
+  var csr = $("input[name=csrfmiddlewaretoken]").val().trim();
  
-  if (saham_dibeli<1){
-    alert("Minimal pembelian 1 lembar");
-    document.getElementById("saldo_kurang").innerHTML = "Minimal pembelian 1 lembar";
-  }else if(sisa_lembar<0){
-      alert("Saham yang tersisa pada perusahaan ini hanya " + saham_sisa +" lembar");
-      document.getElementById("saldo_kurang").innerHTML = "Saham yang tersisa pada perusahaan ini hanya " + saham_sisa +" lembar";
-  }
-  else if(saldo_temp<0){
-      alert("Saldo Anda tidak cukup 😢");
-      document.getElementById("saldo_kurang").innerHTML = "Saldo Anda tidak cukup 😢";
-      
-  } else{
       if (saham_dibeli) {
-          saldo=parseInt(saldo_temp);
-          saham_sisa=sisa_lembar;
           // Create Ajax Call
           $.ajax({
               url: '/mulai-invest/ajax/beli-saham',
+              method: "POST",
               data: {
                   'id': id_comp,
                   'jumlah_lembar_saham': saham_dibeli,
-                  'saldo': saldo
+                  csrfmiddlewaretoken: csr
               },
               dataType: 'json',
               success: function (data) {
-                  if (data.user) {
-                      update_saldo();
+                  if (data.user.status === 'success') {
+                      update_saldo2(data.user);
+                      document.getElementById("saldo_kurang").innerHTML = "Transaksi Anda berhasil 🥳";
+                      $('form#beli_saham').trigger("reset");
+                  } else{
+                      alert(data.user.message);
+                      document.getElementById("saldo_kurang").innerHTML = data.user.message;
                   }
               }
           });
-
       } else {
           alert("All fields must have a valid value.");
       }
-      document.getElementById("saldo_kurang").innerHTML = "Transaksi Anda berhasil 🥳";
-      $('form#beli_saham').trigger("reset");
-  }
   return false;
 });
--- mulai_invest/views.py
+++ mulai_invest/views.py
@@ -1,23 +1,34 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, JsonResponse
-from django.views.generic import View
+from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
+from halaman_toko.authentication_and_authorization import *
 
 from models_app.models.UserAccount import UserAccount
 from models_app.models import Company
 from models_app.models import Stock
+
+# probably delete it later
+from django.contrib.auth.models import User
 # Create your views here.
 
-class UpdateSaldo(View):
-    def  get(self, request):
+@csrf_exempt
+def update_saldo_ajax(request):
+    if request.method == "POST":
         saldoo = request.user.useraccount.saldo
-        saldo1 = int(saldoo)+int(request.GET.get('saldo', None))
+        saldo1 = int(saldoo)+int(request.POST.get('saldo', None))
+        if saldo1>2000000000:
+            return JsonResponse({'user': {'message': 'Saldo maksimal Anda adalah 2 miliar Rupiah', 'status': 'fail'}})
+        if saldo1<0:
+            return JsonResponse({'user': {'message': 'Saldo minimal adalah 0', 'status': 'fail'}})
 
         obj = get_object_or_404(UserAccount, user_model=request.user)
         obj.saldo=saldo1
         obj.save()
 
-        user = {'id':obj.id,'saldo':obj.saldo}
+        user = {'id':obj.id,'saldo':obj.saldo, 'status': 'success'}
 
 
         data = {
@@ -25,42 +36,187 @@ class UpdateSaldo(View):
         }
         return JsonResponse(data)
 
+def beli_saham(request):
+    if request.method=="POST":
+        saldoo = request.user.useraccount.saldo
+        saham_dibeli = int(request.POST.get("jumlah_lembar_saham"))
 
-class BeliSaham(View):
-    def  get(self, request):
-        saldo1 = int(request.GET.get("saldo", None))
-        company_id= int(request.GET.get("id", None))
+        if saham_dibeli < 1:
+            return JsonResponse({'user': {'message': 'Minimal pembelian 1 lembar', 'status': 'fail'}})
+
+        company_id= int(request.POST.get("id", None))
         company_obj=Company.objects.filter(id=company_id).first()
-        
+
+        company_stock = Stock.objects.filter(company=company_obj)
+        lembar_saham_terjual = 0
+        for stock in company_stock:
+            lembar_saham_terjual += stock.jumlah_lembar_saham
+
+        sisa_lembar = int(company_obj.jumlah_lembar) - int(lembar_saham_terjual)
+        sisa_lembar_after = int(sisa_lembar) - int(saham_dibeli)
+        if sisa_lembar_after < 0:
+            return JsonResponse({'user': {'message': 'Saham yang tersisa pada perusahaan ini hanya ' + str(sisa_lembar) + ' lembar', 'status': 'fail'}})
+
+        lembar_saham_terjual += saham_dibeli
+
+        saldo1 = int(saldoo) - int(saham_dibeli) * int(company_obj.nilai_lembar_saham)
+        if saldo1 < 0:
+            return JsonResponse({'user': {'message': 'Saldo Anda tidak cukup 😢', 'status': 'fail'}})
         
         profile_obj = get_object_or_404(UserAccount, user_model=request.user)
         profile_obj.saldo=saldo1
         profile_obj.save()
+
+        stock_obj = None
         
         is_exist = Stock.objects.filter(holder=request.user, company=company_obj)
-        if(is_exist.first() is not None):
+        if(is_exist.first()):
             
-            jumlah_lembar_saham1 = (request.GET.get('jumlah_lembar_saham', None))
-            stock_obj = get_object_or_404(Stock, holder=request.user,company=company_obj)
+            jumlah_lembar_saham1 = (request.POST.get('jumlah_lembar_saham', None))
+            stock_obj = is_exist.first()
             jumlah_lembar_saham = int(stock_obj.jumlah_lembar_saham)
             jumlah_lembar_saham+=int(jumlah_lembar_saham1)
             stock_obj.jumlah_lembar_saham=jumlah_lembar_saham
             stock_obj.save()
 
         else:
-            jumlah_lembar_saham1 = int(request.GET.get('jumlah_lembar_saham', None))
+            jumlah_lembar_saham1 = int(request.POST.get('jumlah_lembar_saham', None))
             stock_obj = Stock.objects.create(
                 holder = request.user,
                 company = company_obj,
                 jumlah_lembar_saham = jumlah_lembar_saham1
             )
 
-        user = {'id':profile_obj.id,'saldo':profile_obj.saldo}
-
-        data = {
-            'user': user
+        user = {
+            'saldo':profile_obj.saldo, 
+            'saham_tersisa': sisa_lembar_after, 
+            'saham_tanam': stock_obj.jumlah_lembar_saham,
+            'saham_terjual': lembar_saham_terjual,
+            'status': 'success'
         }
+
+        data = {'user': user}
         return JsonResponse(data)
+
+@csrf_exempt
+def get_invest_stuff(request):
+    company_id= (request.GET.get("id", None))
+    if(company_id is None):
+        return HttpResponse("Please specify the id")
+    company_id = int(company_id)
+    company_obj_all = list(Company.objects.filter(id=company_id).only('deskripsi'))
+    company_obj=Company.objects.filter(id=company_id).first()
+    if(company_obj is None):
+        return HttpResponse("Sorry, the id you're trying to reach is invalid")
+    mulai_invest={}
+    mulai_invest['company']=  serializers.serialize('json', company_obj_all)
+    
+    # zz = list(company_obj.companyphoto_set.all().order_by("img_index"))
+    # mulai_invest['company_photos']= serializers.serialize('json', zz)
+
+    mulai_invest['company_photos'] = [i.img.url for i in company_obj.companyphoto_set.all().order_by("img_index")]
+
+    mulai_invest['owner_account']=company_obj.pemilik_usaha.account.user_model.username
+    
+    company_stock = Stock.objects.filter(company=company_obj)
+    lembar_saham_terjual = 0
+    for stock in company_stock:
+        lembar_saham_terjual += stock.jumlah_lembar_saham
+
+    mulai_invest['saham_terjual']=lembar_saham_terjual
+    mulai_invest['saham_tersisa']=company_obj.jumlah_lembar - lembar_saham_terjual
+    pengguna = User.objects.filter(id=1)[0]
+    is_exist = Stock.objects.filter(holder=pengguna, company=company_obj)
+    if(is_exist.first()):
+        mulai_invest['lembar_dimiliki']=is_exist.first().jumlah_lembar_saham
+    else:
+        mulai_invest['lembar_dimiliki']=0
+    return JsonResponse(mulai_invest)
+
+@csrf_exempt
+def get_saldo(request):
+    userAcc = UserAccount.objects.filter(id=request.POST.get("userId", None))
+    userSaldo = userAcc[0].saldo
+    return JsonResponse({'saldo':userSaldo})
+
+@csrf_exempt
+def update_saldo_flutter(request):
+    if request.method == "POST":
+        userId = int(request.POST.get('userId', None))
+        userAcc = UserAccount.objects.filter(id=userId)
+        saldoo = userAcc[0].saldo
+        saldo1 = int(saldoo)+int(request.POST.get('saldo', None))
+        if saldo1>2000000000:
+            return JsonResponse({'message': 'Saldo maksimal Anda adalah 2 miliar Rupiah', 'status': 'fail'})
+        if saldo1<0:
+            return JsonResponse({'message': 'Saldo minimal adalah 0', 'status': 'fail'})
+
+        obj = userAcc[0]
+        obj.saldo=saldo1
+        obj.save()
+
+        user = {'id':obj.id,'saldo':obj.saldo, 'status': 'success'}
+        return JsonResponse(user)
+
+@csrf_exempt
+def beli_saham_flutter(request):
+    if request.method=="POST":
+        userId = int(request.POST.get('userId', None))
+        userAcc = UserAccount.objects.filter(id=userId)[0]
+        saldoo = userAcc.saldo
+        saham_dibeli = int(request.POST.get("jumlahLembarSaham"))
+
+        company_id= int(request.POST.get("companyId", None))
+        company_obj=Company.objects.filter(id=company_id).first()
+
+        company_stock = Stock.objects.filter(company=company_obj)
+        lembar_saham_terjual = 0
+        for stock in company_stock:
+            lembar_saham_terjual += stock.jumlah_lembar_saham
+
+        sisa_lembar = int(company_obj.jumlah_lembar) - int(lembar_saham_terjual)
+        sisa_lembar_after = int(sisa_lembar) - int(saham_dibeli)
+        if sisa_lembar_after < 0:
+            return JsonResponse({'message': 'Saham yang tersisa pada perusahaan ini hanya ' + str(sisa_lembar) + ' lembar', 'status': 'fail'})
+
+        lembar_saham_terjual += saham_dibeli
+
+        saldo1 = int(saldoo) - int(saham_dibeli) * int(company_obj.nilai_lembar_saham)
+        if saldo1 < 0:
+            return JsonResponse({'message': 'Saldo Anda tidak cukup :(', 'status': 'fail'})
+        
+        profile_obj = userAcc
+        profile_obj.saldo=saldo1
+        profile_obj.save()
+
+        stock_obj = None
+        
+        pengguna = userAcc.user_model
+        is_exist = Stock.objects.filter(holder=pengguna, company=company_obj)
+        if(is_exist.first()):
+            jumlah_lembar_saham1 = (request.POST.get('jumlahLembarSaham', None))
+            stock_obj = is_exist.first()
+            jumlah_lembar_saham = int(stock_obj.jumlah_lembar_saham)
+            jumlah_lembar_saham+=int(jumlah_lembar_saham1)
+            stock_obj.jumlah_lembar_saham=jumlah_lembar_saham
+            stock_obj.save()
+
+        else:
+            jumlah_lembar_saham1 = int(request.POST.get('jumlahLembarSaham', None))
+            stock_obj = Stock.objects.create(
+                holder = userAcc,
+                company = company_obj,
+                jumlah_lembar_saham = jumlah_lembar_saham1
+            )
+
+        user = {
+            'saldo':profile_obj.saldo, 
+            'saham_tersisa': sisa_lembar_after, 
+            'saham_tanam': stock_obj.jumlah_lembar_saham,
+            'saham_terjual': lembar_saham_terjual,
+            'status': 'success'
+        }
+        return JsonResponse(user)
 
 @login_required(login_url='/start-web/login')
 def mulai_invest(request):
@@ -75,7 +231,7 @@ def mulai_invest(request):
     mulai_invest['company']=company_obj
     mulai_invest['company_photos']=company_obj.companyphoto_set.all().order_by("img_index")
     mulai_invest['owner_account']=company_obj.pemilik_usaha.account
-    
+
     company_stock = Stock.objects.filter(company=company_obj)
     lembar_saham_terjual = 0
     for stock in company_stock:
