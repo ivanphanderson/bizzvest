7d1ea1dbf09e66ea5e5f55915e2049f67e0e3dde
Ivan Phanderson <ivan.phanderson@ui.ac.id>
2021-12-31 09:35:05 +0000

    Fix bug

--- mulai_invest/urls.py
+++ mulai_invest/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('ajax/update-saldo',  views.update_saldo_ajax, name='update_saldo'),
     path('ajax/beli-saham',  views.beli_saham, name='beli_saham'),
     path('flutter/get-saldo', views.get_saldo, name='get_saldo'),
-    path('invest', views.get_invest_stuff, name='invest'),
+    path('flutter/invest', views.get_invest_stuff, name='invest'),
     path('flutter/update-saldo', views.update_saldo_flutter, name='update_saldo_flutter'),
     path('flutter/beli-saham', views.beli_saham_flutter, name='beli_saham_flutter'),
 ]
