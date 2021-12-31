67208dd37819b4113c0d489253d62521e002fccc
Ivan Phanderson <ivan.phanderson@ui.ac.id>
2021-12-31 19:19:04 +0700

    change method

--- mulai_invest/views.py
+++ mulai_invest/views.py
@@ -9,6 +9,7 @@ from halaman_toko.authentication_and_authorization import *
 from models_app.models.UserAccount import UserAccount
 from models_app.models import Company
 from models_app.models import Stock
+import traceback
 
 # probably delete it later
 from django.contrib.auth.models import User
@@ -124,7 +125,7 @@ def get_invest_stuff(request):
         mulai_invest['saham_tersisa']=company_obj.jumlah_lembar - lembar_saham_terjual
         # pengguna = User.objects.filter(id=)[0]
         # print(request.POST)
-        pengguna = UserAccount.objects.filter(id=int(request.POST.get("userId", None)))[0].user_model
+        pengguna = UserAccount.objects.filter(id=int(request.GET.get("userId", None)))[0].user_model
         is_exist = Stock.objects.filter(holder=pengguna, company=company_obj)
         if(is_exist.first()):
             mulai_invest['lembar_dimiliki']=is_exist.first().jumlah_lembar_saham
@@ -132,12 +133,12 @@ def get_invest_stuff(request):
             mulai_invest['lembar_dimiliki']=0
         return JsonResponse(mulai_invest)
     except:
-        return HttpResponse('fail')
+        return HttpResponse((traceback.format_exc()) +'\n\nrequest.POST = ' + str(request.POST))
 
 @csrf_exempt
 def get_saldo(request):
     try:
-        userAcc = UserAccount.objects.filter(id=request.POST.get("userId", None))
+        userAcc = UserAccount.objects.filter(id=request.GET.get("userId", None))
         userSaldo = userAcc[0].saldo
         return JsonResponse({'saldo':userSaldo})
     except:
@@ -146,86 +147,86 @@ def get_saldo(request):
 @csrf_exempt
 def update_saldo_flutter(request):
     try:
-        if request.method == "POST":
-            userId = (request.POST.get('userId', None))
-            userAcc = UserAccount.objects.filter(id=userId)
-            saldoo = userAcc[0].saldo
-            saldo1 = int(saldoo)+int(request.POST.get('saldo', None))
-            if saldo1>2000000000:
-                return JsonResponse({'message': 'Saldo maksimal Anda adalah 2 miliar Rupiah', 'status': 'fail'})
-            if saldo1<0:
-                return JsonResponse({'message': 'Saldo minimal adalah 0', 'status': 'fail'})
+        # if request.method == "POST":
+        userId = (request.GET.get('userId', None))
+        userAcc = UserAccount.objects.filter(id=userId)
+        saldoo = userAcc[0].saldo
+        saldo1 = int(saldoo)+int(request.GET.get('saldo', None))
+        if saldo1>2000000000:
+            return JsonResponse({'message': 'Saldo maksimal Anda adalah 2 miliar Rupiah', 'status': 'fail'})
+        if saldo1<0:
+            return JsonResponse({'message': 'Saldo minimal adalah 0', 'status': 'fail'})
 
-            obj = userAcc[0]
-            obj.saldo=saldo1
-            obj.save()
+        obj = userAcc[0]
+        obj.saldo=saldo1
+        obj.save()
 
-            user = {'id':obj.id,'saldo':obj.saldo, 'status': 'success'}
-            return JsonResponse(user)
+        user = {'id':obj.id,'saldo':obj.saldo, 'status': 'success'}
+        return JsonResponse(user)
     except:
         return HttpResponse('fail')
 
 @csrf_exempt
 def beli_saham_flutter(request):
     try:
-        if request.method=="POST":
-            userId = int(request.POST.get('userId', None))
-            userAcc = UserAccount.objects.filter(id=userId)[0]
-            saldoo = userAcc.saldo
-            saham_dibeli = int(request.POST.get("jumlahLembarSaham"))
+        # if request.method=="POST":
+        userId = int(request.GET.get('userId', None))
+        userAcc = UserAccount.objects.filter(id=userId)[0]
+        saldoo = userAcc.saldo
+        saham_dibeli = int(request.GET.get("jumlahLembarSaham"))
 
-            company_id= int(request.POST.get("companyId", None))
-            company_obj=Company.objects.filter(id=company_id).first()
+        company_id= int(request.GET.get("companyId", None))
+        company_obj=Company.objects.filter(id=company_id).first()
 
-            company_stock = Stock.objects.filter(company=company_obj)
-            lembar_saham_terjual = 0
-            for stock in company_stock:
-                lembar_saham_terjual += stock.jumlah_lembar_saham
+        company_stock = Stock.objects.filter(company=company_obj)
+        lembar_saham_terjual = 0
+        for stock in company_stock:
+            lembar_saham_terjual += stock.jumlah_lembar_saham
 
-            sisa_lembar = int(company_obj.jumlah_lembar) - int(lembar_saham_terjual)
-            sisa_lembar_after = int(sisa_lembar) - int(saham_dibeli)
-            if sisa_lembar_after < 0:
-                return JsonResponse({'message': 'Saham yang tersisa pada perusahaan ini hanya ' + str(sisa_lembar) + ' lembar', 'status': 'fail'})
+        sisa_lembar = int(company_obj.jumlah_lembar) - int(lembar_saham_terjual)
+        sisa_lembar_after = int(sisa_lembar) - int(saham_dibeli)
+        if sisa_lembar_after < 0:
+            return JsonResponse({'message': 'Saham yang tersisa pada perusahaan ini hanya ' + str(sisa_lembar) + ' lembar', 'status': 'fail'})
 
-            lembar_saham_terjual += saham_dibeli
+        lembar_saham_terjual += saham_dibeli
 
-            saldo1 = int(saldoo) - int(saham_dibeli) * int(company_obj.nilai_lembar_saham)
-            if saldo1 < 0:
-                return JsonResponse({'message': 'Saldo Anda tidak cukup :(', 'status': 'fail'})
-            
-            profile_obj = userAcc
-            profile_obj.saldo=saldo1
-            profile_obj.save()
+        saldo1 = int(saldoo) - int(saham_dibeli) * int(company_obj.nilai_lembar_saham)
+        if saldo1 < 0:
+            return JsonResponse({'message': 'Saldo Anda tidak cukup :(', 'status': 'fail'})
+        
+        profile_obj = userAcc
+        profile_obj.saldo=saldo1
+        profile_obj.save()
 
-            stock_obj = None
-            
-            pengguna = userAcc.user_model
-            is_exist = Stock.objects.filter(holder=pengguna, company=company_obj)
-            if(is_exist.first()):
-                jumlah_lembar_saham1 = (request.POST.get('jumlahLembarSaham', None))
-                stock_obj = is_exist.first()
-                jumlah_lembar_saham = int(stock_obj.jumlah_lembar_saham)
-                jumlah_lembar_saham+=int(jumlah_lembar_saham1)
-                stock_obj.jumlah_lembar_saham=jumlah_lembar_saham
-                stock_obj.save()
+        stock_obj = None
+        
+        pengguna = userAcc.user_model
+        is_exist = Stock.objects.filter(holder=pengguna, company=company_obj)
+        if(is_exist.first()):
+            jumlah_lembar_saham1 = (request.GET.get('jumlahLembarSaham', None))
+            stock_obj = is_exist.first()
+            jumlah_lembar_saham = int(stock_obj.jumlah_lembar_saham)
+            jumlah_lembar_saham+=int(jumlah_lembar_saham1)
+            stock_obj.jumlah_lembar_saham=jumlah_lembar_saham
+            stock_obj.save()
 
-            else:
-                jumlah_lembar_saham1 = int(request.POST.get('jumlahLembarSaham', None))
-                stock_obj = Stock.objects.create(
-                    holder = userAcc,
-                    company = company_obj,
-                    jumlah_lembar_saham = jumlah_lembar_saham1
-                )
+        else:
+            jumlah_lembar_saham1 = int(request.GET.get('jumlahLembarSaham', None))
+            stock_obj = Stock.objects.create(
+                holder = userAcc,
+                company = company_obj,
+                jumlah_lembar_saham = jumlah_lembar_saham1
+            )
 
-            user = {
-                'saldo':profile_obj.saldo, 
-                'saham_tersisa': sisa_lembar_after, 
-                'saham_tanam': stock_obj.jumlah_lembar_saham,
-                'saham_terjual': lembar_saham_terjual,
-                'status': 'success'
-            }
-            return JsonResponse(user)
-        return HttpResponse("hi")
+        user = {
+            'saldo':profile_obj.saldo, 
+            'saham_tersisa': sisa_lembar_after, 
+            'saham_tanam': stock_obj.jumlah_lembar_saham,
+            'saham_terjual': lembar_saham_terjual,
+            'status': 'success'
+        }
+        return JsonResponse(user)
+        # return HttpResponse("hi")
     except:
         return HttpResponse('fail')
 
